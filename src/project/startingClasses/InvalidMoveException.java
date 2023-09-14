26bad646bb8ecf36846600fc00ebb3986905f9dc
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-09-14 13:17:15 -0700

    feat: initial commit: add starter code

---
+++ src/project/startingClasses/InvalidMoveException.java
@@ -0,0 +1,14 @@
+package startingClasses;
+
+/**
+ * Indicates an invalid move was made in a game
+ */
+public class InvalidMoveException extends Exception {
+
+    public InvalidMoveException() {
+    }
+
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+}
