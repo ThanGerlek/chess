fc0d9e3e939e232c4208d82c59d5b11fc4953d67
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-09-28 22:41:23 -0700

    feat(piece): implement Queen's pieceMoves() using RelativeChessMove

--- src/project/chess/ChessPieceImpl.java
+++ src/project/chess/ChessPieceImpl.java
@@ -4,6 +4,7 @@ package chess;
 import chess.pieces.*;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 public abstract class ChessPieceImpl implements ChessPiece {
 
@@ -56,6 +57,40 @@ public abstract class ChessPieceImpl implements ChessPiece {
         if (color == ChessGame.TeamColor.WHITE)
             return shift(position, deltaRow, deltaCol);
         return shift(position, -deltaRow, -deltaCol);
+    }
+
+    /**
+     * Returns a Collection of all ChessMoves obtained by repeatedly applying
+     * the given RelativeChessMove to the given starting ChessPosition any
+     * number of times until an invalid move is made. Application stops when
+     * applying the RelativeChessMove again would land either outside the
+     * board, on an occupied space, or back on the start position (the start
+     * position is not included in the Collection). If the final move is a
+     * capture (one that ends on a space occupied by an enemy piece), it is
+     * included in the Collection, but counts as an end condition and the
+     * relativeMove is not applied again.
+     * @param board the ChessBoard.
+     * @param startPosition the starting ChessPosition.
+     * @param relativeMove the RelativeChessMove to apply to the startPosition.
+     * @return a Collection of ChessMoves made by repeatedly applying relativeMove.
+     */
+    protected Collection<ChessMove> getMovesFromRepeatedRelativeMove(ChessBoard board, ChessPosition startPosition, RelativeChessMove relativeMove) {
+        Collection<ChessMove> moves = new LinkedList<>();
+
+        ChessPosition currentEndPosition = relativeMove.apply(startPosition);
+
+        while (isValidEmptySpace(board, currentEndPosition)
+                && !currentEndPosition.equals(startPosition)) {
+            moves.add(new ChessMoveImpl(startPosition, currentEndPosition));
+            currentEndPosition = relativeMove.apply(currentEndPosition);
+        }
+
+        ChessPiece previousOccupant = board.getPiece(currentEndPosition);
+        if (previousOccupant != null && previousOccupant.getTeamColor() != getTeamColor()) {
+            moves.add(new ChessMoveImpl(startPosition, currentEndPosition));
+        }
+
+        return moves;
     }
 
     /**
