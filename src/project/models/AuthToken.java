949b5b4af1a710422a151ce6a5247482ac85b18f
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-10-04 13:07:58 -0700

    feat(server): add AuthToken, Game, and User model objects

---
+++ src/project/models/AuthToken.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * An authorization token for the given user.
+ *
+ * @param authToken the unique token string
+ * @param username  the user this AuthToken represents
+ */
+public record AuthToken(String authToken, String username) {
+}
---
+++ src/project/models/Game.java
@@ -0,0 +1,66 @@
+package models;
+
+import chess.ChessGame;
+
+/**
+ * A model object representing the core data of a chess game.
+ */
+public class Game {
+
+    private final int gameID;
+    private ChessGame chessGame;
+    private String whiteUsername;
+    private String blackUsername;
+    private String gameName;
+
+    /**
+     * Creates a new Game object.
+     *
+     * @param gameID    the unique ID of this Game
+     * @param gameName  a human-readable name for this Game
+     * @param chessGame a ChessGame object representing the current game state
+     */
+    public Game(int gameID, String gameName, ChessGame chessGame) {
+        this.gameID = gameID;
+        this.gameName = gameName;
+        this.chessGame = chessGame;
+        this.whiteUsername = "";
+        this.blackUsername = "";
+    }
+
+    public int gameID() {
+        return gameID;
+    }
+
+    public ChessGame chessGame() {
+        return chessGame;
+    }
+
+    public void setChessGame(ChessGame chessGame) {
+        this.chessGame = chessGame;
+    }
+
+    public String whiteUsername() {
+        return whiteUsername;
+    }
+
+    public void setWhiteUsername(String whiteUsername) {
+        this.whiteUsername = whiteUsername;
+    }
+
+    public String blackUsername() {
+        return blackUsername;
+    }
+
+    public void setBlackUsername(String blackUsername) {
+        this.blackUsername = blackUsername;
+    }
+
+    public String gameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+}
