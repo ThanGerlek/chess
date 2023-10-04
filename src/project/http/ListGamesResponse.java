0bfb3d0de776d0371342d72ac4ace4997772824f
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-10-04 13:13:56 -0700

    feat(api): add objects for ListGames HTTP responses

---
+++ src/project/http/ListGamesResponse.java
@@ -0,0 +1,14 @@
+package http;
+
+/**
+ * A record representing an HTTP response to a List Games service call.
+ *
+ * @param games   a list of games currently in the database
+ * @param message the response message
+ */
+public record ListGamesResponse(GameListItem[] games, String message) {
+}
+
+//    Success response	[200] { "games": [{"gameID": 1234, "whiteUsername":"", "blackUsername":"", "gameName:""} ]}
+//    Failure response	[401] { "message": "Error: unauthorized" }
+//    Failure response	[500] { "message": "Error: description" }
