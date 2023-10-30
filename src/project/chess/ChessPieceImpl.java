c1686b57839d767dffed65d5f1a5c97d69955cbe
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-10-30 16:54:03 -0700

    formatting

--- src/project/chess/ChessPieceImpl.java
+++ src/project/chess/ChessPieceImpl.java
@@ -21,23 +21,8 @@ public abstract class ChessPieceImpl implements ChessPiece {
     }
 
     /**
-     * Returns a version of the given ChessPosition shifted by the given amounts.
-     * Positive values shift up and right relative to the white player.
-     *
-     * @param position the starting ChessPosition.
-     * @param deltaRow the amount to shift the row.
-     * @param deltaCol the amount to shift the column.
-     * @return a shifted ChessPosition.
-     */
-    protected static ChessPosition shift(ChessPosition position, int deltaRow, int deltaCol) {
-        return new ChessPositionImpl(position.getRow() + deltaRow, position.getColumn() + deltaCol);
-    }
-
-    /**
-     * Returns a version of the given ChessPosition shifted by the given
-     * amounts towards the opposing side.
-     * Positive values shift up and right relative to the player who owns
-     * this piece.
+     * Returns a version of the given ChessPosition shifted by the given amounts towards the opposing side. Positive
+     * values shift up and right relative to the player who owns this piece.
      *
      * @param position the starting ChessPosition.
      * @param deltaRow the amount to shift the row.
@@ -53,14 +38,11 @@ public abstract class ChessPieceImpl implements ChessPiece {
     }
 
     /**
-     * Returns a Collection of all ChessMoves obtained by repeatedly applying
-     * the given RelativeChessMove to the given starting ChessPosition any
-     * number of times until an invalid move is made. Application stops when
-     * applying the RelativeChessMove again would land either outside the
-     * board, on an occupied space, or back on the start position (the start
-     * position is not included in the Collection). If the final move is a
-     * capture (one that ends on a space occupied by an enemy piece), it is
-     * included in the Collection, but counts as an end condition and the
+     * Returns a Collection of all ChessMoves obtained by repeatedly applying the given RelativeChessMove to the given
+     * starting ChessPosition any number of times until an invalid move is made. Application stops when applying the
+     * RelativeChessMove again would land either outside the board, on an occupied space, or back on the start position
+     * (the start position is not included in the Collection). If the final move is a capture (one that ends on a space
+     * occupied by an enemy piece), it is included in the Collection, but counts as an end condition and the
      * relativeMove is not applied again.
      *
      * @param board         the ChessBoard.
