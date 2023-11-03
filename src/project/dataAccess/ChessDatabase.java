ed5d778f7acb8aeedcb87128ad61f1262cb70195
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-02 20:47:57 -0700

    fix(db): replace query() with queryForString()
    Returning a general ResultSet wasn't working, so it only returns a list of Strings.

--- src/project/dataAccess/ChessDatabase.java
+++ src/project/dataAccess/ChessDatabase.java
@@ -3,6 +3,7 @@ package dataAccess;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ChessDatabase extends Database {
 
@@ -42,20 +43,20 @@ public class ChessDatabase extends Database {
         });
     }
 
+    /**
+     * Returns true if the SQL query has at least one result.
+     *
+     * @param sqlString the SQL query to execute
+     * @param sp        a lambda that inserts any parameters
+     * @return true if the SQL query has at least one result
+     */
     public boolean booleanQuery(String sqlString, StatementPreparer sp) throws DataAccessException {
-        try (ResultSet rs = query(sqlString, sp)) {
-            return rs.next();
-        } catch (SQLException e) {
-            System.out.println("Failed to read ResultSet on boolean SQL query: `" + sqlString + "`");
-            throw new DataAccessException(e.getMessage());
-        }
-    }
-
-    public ResultSet query(String sqlString, StatementPreparer sp) throws DataAccessException {
         Connection conn = getConnection();
         try (var preparedStatement = conn.prepareStatement(sqlString)) {
             sp.prepare(preparedStatement);
-            return preparedStatement.executeQuery();
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                return rs.next();
+            }
         } catch (SQLException e) {
             System.out.println("Failed to run executeQuery() on SQL String: `" + sqlString + "`");
             throw new DataAccessException(e.getMessage());
@@ -64,15 +65,40 @@ public class ChessDatabase extends Database {
         }
     }
 
-    public ResultSet queryWithParam(String sqlString, String param) throws DataAccessException {
-        return query(sqlString, preparedStatement -> {
-            preparedStatement.setString(1, param);
-        });
-    }
-
     public boolean booleanQueryWithParam(String sqlString, int param) throws DataAccessException {
         return booleanQuery(sqlString, preparedStatement -> {
             preparedStatement.setInt(1, param);
         });
+    }
+
+    /**
+     * Return a list of resultColumnLabel values from the rows returned by the SQL query.
+     *
+     * @param sqlString         the SQL query to execute
+     * @param sp                a lambda that inserts any parameters
+     * @param resultColumnLabel the SQL column to return values from
+     * @return a list of resultColumnLabel values from rows returned by the query
+     */
+    public ArrayList<String> queryForString(String resultColumnLabel, String sqlString, StatementPreparer sp)
+            throws DataAccessException {
+        Connection conn = getConnection();
+        try (var preparedStatement = conn.prepareStatement(sqlString)) {
+            sp.prepare(preparedStatement);
+
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                ArrayList<String> results = new ArrayList<>();
+                while (rs.next()) {
+                    results.add(rs.getString(resultColumnLabel));
+                }
+                return results;
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Failed to run executeQuery() on SQL String: `" + sqlString + "`");
+            throw new DataAccessException(e.getMessage());
+        } finally {
+            returnConnection(conn);
+        }
     }
 }
