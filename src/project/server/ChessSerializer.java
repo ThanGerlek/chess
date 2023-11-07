3352fbe3098e833ec1d0f53f738ae7e826ff001f
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-06 22:10:55 -0700

    enhance: reduce duplicated code

--- src/project/server/ChessSerializer.java
+++ src/project/server/ChessSerializer.java
@@ -6,26 +6,29 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 
+import java.util.LinkedList;
+
 public class ChessSerializer {
 
     public static Gson gson() {
-        var boardRTAF = RuntimeTypeAdapterFactory.of(ChessBoard.class, "type").registerSubtype(ChessBoardImpl.class);
-        var positionRTAF =
-                RuntimeTypeAdapterFactory.of(ChessPosition.class, "type").registerSubtype(ChessPositionImpl.class);
-        var moveRTAF = RuntimeTypeAdapterFactory.of(ChessMove.class, "type").registerSubtype(ChessMoveImpl.class);
-        var pieceRTAF = RuntimeTypeAdapterFactory.of(ChessPiece.class, "pieceType")
+        LinkedList<RuntimeTypeAdapterFactory> factories = new LinkedList<>();
+
+        factories.push(RuntimeTypeAdapterFactory.of(ChessBoard.class, "type").registerSubtype(ChessBoardImpl.class));
+        factories.push(RuntimeTypeAdapterFactory.of(ChessMove.class, "type").registerSubtype(ChessMoveImpl.class));
+        factories.push(
+                RuntimeTypeAdapterFactory.of(ChessPosition.class, "type").registerSubtype(ChessPositionImpl.class));
+        factories.push(RuntimeTypeAdapterFactory.of(ChessPiece.class, "pieceType")
                 .registerSubtype(King.class)
                 .registerSubtype(Queen.class)
                 .registerSubtype(Knight.class)
                 .registerSubtype(Rook.class)
                 .registerSubtype(Bishop.class)
-                .registerSubtype(Pawn.class);
+                .registerSubtype(Pawn.class));
 
         GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapterFactory(boardRTAF);
-        builder.registerTypeAdapterFactory(positionRTAF);
-        builder.registerTypeAdapterFactory(moveRTAF);
-        builder.registerTypeAdapterFactory(pieceRTAF);
+        for (RuntimeTypeAdapterFactory factory : factories) {
+            builder.registerTypeAdapterFactory(factory);
+        }
         return builder.create();
     }
 }
