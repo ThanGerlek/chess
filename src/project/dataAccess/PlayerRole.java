2285f6aa99fcf2bce399c9e9765078197d32332b
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-07 13:00:26 -0700

    enhance: simplified PlayerRole internals

--- src/project/dataAccess/PlayerRole.java
+++ src/project/dataAccess/PlayerRole.java
@@ -1,12 +1,9 @@
 package dataAccess;
 
 public class PlayerRole {
-    private static final String WHITE_STRING = "white";
-    public static final PlayerRole WHITE_PLAYER = new PlayerRole(WHITE_STRING);
-    private static final String BLACK_STRING = "black";
-    public static final PlayerRole BLACK_PLAYER = new PlayerRole(BLACK_STRING);
-    private static final String SPECTATOR_STRING = "spectator";
-    public static final PlayerRole SPECTATOR = new PlayerRole(SPECTATOR_STRING);
+    public static final PlayerRole WHITE_PLAYER = new PlayerRole("WHITE_PLAYER");
+    public static final PlayerRole BLACK_PLAYER = new PlayerRole("BLACK_PLAYER");
+    public static final PlayerRole SPECTATOR = new PlayerRole("SPECTATOR");
     private final String roleString;
 
     private PlayerRole(String roleString) {
@@ -15,18 +12,18 @@ public class PlayerRole {
 
     public static String roleToString(PlayerRole role) {
         if (role == null) {
-            return SPECTATOR_STRING;
+            return SPECTATOR.roleString;
         } else {
             return role.roleString;
         }
     }
 
     public static PlayerRole stringToRole(String roleString) {
-        if (roleString == null || roleString.isEmpty() || SPECTATOR_STRING.equals(roleString)) {
+        if (roleString == null || roleString.isEmpty() || SPECTATOR.roleString.equals(roleString)) {
             return SPECTATOR;
-        } else if (WHITE_STRING.equals(roleString)) {
+        } else if (WHITE_PLAYER.roleString.equals(roleString)) {
             return WHITE_PLAYER;
-        } else if (BLACK_STRING.equals(roleString)) {
+        } else if (BLACK_PLAYER.roleString.equals(roleString)) {
             return BLACK_PLAYER;
         } else {
             String msg = String.format("Called stringToRole() with an unrecognized role string: '%s'", roleString);
