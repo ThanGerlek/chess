0d7d9cd0945a50e25d081f2feb5cdac91bbc95db
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-08 20:33:42 -0700

    enhance(test): remove unused code

--- src/test/server/services/ServiceTest.java
+++ src/test/server/services/ServiceTest.java
@@ -1,9 +1,6 @@
 package server.services;
 
 import dataAccess.*;
-import server.AuthToken;
-import server.Game;
-import server.User;
 
 abstract class ServiceTest {
     protected AuthDAO authDAO;
@@ -16,19 +13,4 @@ abstract class ServiceTest {
         gameDAO = new MemoryGameDAO(userDAO);
     }
 
-    void fillDatabases() throws DataAccessException {
-        User user1 = new User("user1", "pass1", "mail1");
-        User user2 = new User("user2", "pass2", "mail2");
-        Game game1 = new Game(1, "game1");
-        Game game2 = new Game(2, "game2");
-        AuthToken token1 = new AuthToken("1234", "user1");
-        AuthToken token2 = new AuthToken("2468", "user2");
-
-        userDAO.insertNewUser(user1);
-        userDAO.insertNewUser(user2);
-        gameDAO.insertNewGame(game1);
-        gameDAO.insertNewGame(game2);
-        authDAO.addAuthToken(token1);
-        authDAO.addAuthToken(token2);
-    }
 }
