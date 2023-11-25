39865b2cc7a01443a28a8a65c29d5323d8478891
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-24 19:13:35 -0700

    feat: add http logic to ServerFacade

--- client/src/main/client/ServerFacade.java
+++ client/src/main/client/ServerFacade.java
@@ -1,6 +1,16 @@
 package client;
 
+import com.google.gson.Gson;
 import http.CreateGameRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class ServerFacade {
     private String serverURL;
@@ -38,6 +48,55 @@ public class ServerFacade {
 
     public void observeGame() {
         // TODO
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+        try {
+            HttpURLConnection http = setUpConnection(method, path);
+            writeRequestBody(request, http);
+            http.connect();
+            throwIfUnsuccessful(http);
+            return readResponseBody(http, responseClass);
+        } catch (Exception e) {
+            // TODO!
+            System.out.println("Problem! Failed to make a request!");
+            throw e;
+        }
+    }
+
+    private HttpURLConnection setUpConnection(String method, String path) throws URISyntaxException, IOException {
+        URL url = (new URI(serverURL + path)).toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod(method);
+        http.setDoOutput(true); // TODO What is this?
+        return http;
+    }
+
+    private static void writeRequestBody(Object request, HttpURLConnection http) throws IOException {
+        http.addRequestProperty("Content-type", "application/json");
+        String requestData = new Gson().toJson(request);
+        try (OutputStream reqBody = http.getOutputStream()) {
+            reqBody.write(requestData.getBytes());
+        }
+    }
+
+    private static void throwIfUnsuccessful(HttpURLConnection http) throws Exception {
+        boolean successful = http.getResponseCode() / 100 == 2;
+        if (!successful) {
+            // TODO!
+            throw new Exception("Problem! Got a failure response code!");
+        }
+    }
+
+    private static <T> T readResponseBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream responseBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(responseBody);
+                response = new Gson().fromJson(reader, responseClass);
+            }
+        }
+        return response;
     }
 
 }
