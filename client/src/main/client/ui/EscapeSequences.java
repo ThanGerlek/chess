31c2d9fe963380164d8e7a81c679b7f0ad1ed54f
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-27 22:17:13 -0700

    feat: adapt EscapeSequences

--- client/src/main/client/ui/EscapeSequences.java
+++ client/src/main/client/ui/EscapeSequences.java
@@ -56,7 +56,6 @@ public class EscapeSequences {
     public static final String SET_TEXT_COLOR_BLUE = SET_TEXT_COLOR + "12m";
     public static final String SET_TEXT_COLOR_MAGENTA = SET_TEXT_COLOR + "5m";
     public static final String SET_TEXT_COLOR_WHITE = SET_TEXT_COLOR + "15m";
-    public static final String RESET_TEXT_COLOR = SET_TEXT_COLOR + "0m";
     private static final String SET_BG_COLOR = UNICODE_ESCAPE + "[48;5;";
     public static final String SET_BG_COLOR_BLACK = SET_BG_COLOR + "0m";
     public static final String SET_BG_COLOR_LIGHT_GREY = SET_BG_COLOR + "242m";
@@ -68,9 +67,18 @@ public class EscapeSequences {
     public static final String SET_BG_COLOR_BLUE = SET_BG_COLOR + "12m";
     public static final String SET_BG_COLOR_MAGENTA = SET_BG_COLOR + "5m";
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
-    public static final String RESET_BG_COLOR = SET_BG_COLOR + "0m";
 
     public static String moveCursorToLocation(int x, int y) {
         return UNICODE_ESCAPE + "[" + y + ";" + x + "H";
     }
+
+    // provided code
+    // public static final String RESET_TEXT_COLOR = SET_TEXT_COLOR + "0m";
+    // public static final String RESET_BG_COLOR = SET_BG_COLOR + "0m";
+
+    // theoretically correct code
+    // public static final String RESET_TEXT_COLOR = UNICODE_ESCAPE + "[39m";
+    public static final String RESET_BG_COLOR = UNICODE_ESCAPE + "[49m";
+
+    public static final String RESET_TEXT_AND_BG = UNICODE_ESCAPE + "[0m";
 }
