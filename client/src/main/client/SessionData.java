78d708cf29176180a5dc96afa15963d5f7197327
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-11-30 19:53:26 -0700

    feat: store game data in SessionData

--- client/src/main/client/SessionData.java
+++ client/src/main/client/SessionData.java
@@ -1,9 +1,14 @@
 package client;
 
+import chess.ChessGame;
+
 public class SessionData {
     private AuthorizationRole authRole;
     private String authTokenString;
     private String username;
+    private int gameID;
+    private ChessGame.TeamColor playerColor;
+    private boolean isInGame;
 
     public SessionData() {
         this(AuthorizationRole.GUEST, null, null);
@@ -15,9 +20,8 @@ public class SessionData {
         this.username = username;
     }
 
-    public void clearUserData() {
-        setUserData(null, null);
-        this.authRole = AuthorizationRole.GUEST;
+    public AuthorizationRole getAuthRole() {
+        return authRole;
     }
 
     public void setAuthRole(AuthorizationRole authRole) {
@@ -29,8 +33,9 @@ public class SessionData {
         this.username = username;
     }
 
-    public AuthorizationRole getAuthRole() {
-        return authRole;
+    public void clearUserData() {
+        setUserData(null, null);
+        this.authRole = AuthorizationRole.GUEST;
     }
 
     public String getAuthTokenString() {
@@ -39,5 +44,37 @@ public class SessionData {
 
     public String getUsername() {
         return username;
+    }
+
+    public void setGameData(int gameID, ChessGame.TeamColor playerColor) {
+        this.gameID = gameID;
+        this.playerColor = playerColor;
+        isInGame = true;
+    }
+
+    public void clearGameData() {
+        gameID = -1;
+        playerColor = null;
+        isInGame = false;
+    }
+
+    public int getGameID() {
+        requireInGame();
+        return gameID;
+    }
+
+    public ChessGame.TeamColor getPlayerColor() {
+        requireInGame();
+        return playerColor;
+    }
+
+    public boolean isInGame() {
+        return isInGame;
+    }
+
+    private void requireInGame() {
+        if (!isInGame) {
+            throw new NullPointerException("Tried to get game data when not in a game");
+        }
     }
 }
