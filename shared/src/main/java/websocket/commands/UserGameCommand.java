472636130bdc75f66f4149ebf6554b884c55307f
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2024-09-09 09:53:27 -0700

    update UserGameCommand

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 /**
  * Represents a command a user can send the server over a websocket
- * 
+ *
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
@@ -15,8 +15,7 @@ public class UserGameCommand {
     }
 
     public enum CommandType {
-        JOIN_PLAYER,
-        JOIN_OBSERVER,
+        CONNECT,
         MAKE_MOVE,
         LEAVE,
         RESIGN
@@ -36,10 +35,12 @@ public class UserGameCommand {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (!(o instanceof UserGameCommand))
+        }
+        if (!(o instanceof UserGameCommand)) {
             return false;
+        }
         UserGameCommand that = (UserGameCommand) o;
         return getCommandType() == that.getCommandType() && Objects.equals(getAuthString(), that.getAuthString());
     }
