0a08918415de0b9df87ee81f1ba698b11f344fc4
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2024-09-13 15:54:31 -0700

    feat: err handling for joining with no games

--- client/src/main/java/client/GameJoiner.java
+++ client/src/main/java/client/GameJoiner.java
@@ -6,6 +6,7 @@ import httpConnection.ChessServerFacade;
 import httpConnection.FailedConnectionException;
 import httpConnection.FailedResponseException;
 import ui.ConsoleUI;
+import ui.IllegalCommandException;
 import websocket.commands.ConnectGameCommand;
 
 import java.util.ArrayList;
@@ -26,27 +27,35 @@ public class GameJoiner {
 
     public ConnectGameCommand joinGame() throws FailedResponseException, FailedConnectionException,
             CommandCancelException {
-        ui.println("Enter the number for the game you would like to play.");
-        int gameID = selectGame();
-        ChessGame.TeamColor color = selectColor();
+        if (!games.isEmpty()) {
+            ui.println("Enter the number for the game you would like to play.");
+            int gameID = selectGame();
+            ChessGame.TeamColor color = selectColor();
 
-        serverFacade.joinGame(color, gameID, sessionData.getAuthTokenString());
+            serverFacade.joinGame(color, gameID, sessionData.getAuthTokenString());
 
-        sessionData.setGameData(gameID, color);
-        sessionData.setAuthRole(AuthorizationRole.PLAYER);
+            sessionData.setGameData(gameID, color);
+            sessionData.setAuthRole(AuthorizationRole.PLAYER);
 
-        ChessGame.TeamColor playerColor = sessionData.getPlayerColor();
-        return buildConnectGameCommand(playerColor);
+            ChessGame.TeamColor playerColor = sessionData.getPlayerColor();
+            return buildConnectGameCommand(playerColor);
+        } else {
+            throw new IllegalCommandException("Can't join a game: no games were found. Try creating a game first.");
+        }
     }
 
     public ConnectGameCommand observeGame() throws CommandCancelException {
-        ui.println("Enter the number for the game you would like to observe.");
-        int gameID = selectGame();
+        if (!games.isEmpty()) {
+            ui.println("Enter the number for the game you would like to observe.");
+            int gameID = selectGame();
 
-        sessionData.setGameData(gameID, null);
-        sessionData.setAuthRole(AuthorizationRole.OBSERVER);
+            sessionData.setGameData(gameID, null);
+            sessionData.setAuthRole(AuthorizationRole.OBSERVER);
 
-        return buildConnectGameCommand(null);
+            return buildConnectGameCommand(null);
+        } else {
+            throw new IllegalCommandException("Failed to observe: no games are currently being played.");
+        }
     }
 
     private ConnectGameCommand buildConnectGameCommand(ChessGame.TeamColor playerColor) {
