b3a50bcc5e48f44d937d496dc6fa33f3e8d9f8fb
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2024-10-26 14:07:19 -0700

    initial partial Server conversion

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,14 +2,16 @@ package server;
 
 import dataaccess.*;
 import http.MessageResponse;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
 import server.handlers.*;
 import server.webSocket.GameSessionManager;
 import server.webSocket.WebSocketServer;
-import spark.Request;
-import spark.Response;
-import spark.Spark;
 
 public class Server {
+
+    private Javalin app;
 
     private final ClearApplicationHandler clearApplicationHandler;
     private final CreateGameHandler createGameHandler;
@@ -49,36 +51,62 @@ public class Server {
     }
 
     public void stop() {
-        Spark.stop();
-        Spark.awaitStop();
+        app.stop();
+        // TODO? Spark.awaitStop();
     }
 
     public int run(int desiredPort) {
         System.out.println("Starting the server");
 
-        Spark.port(desiredPort);
+        Handler testHandler = (ctx) -> {
+            System.out.println("ctx.method().name() = '" + ctx.method().name() + "'");
+            System.out.println("ctx.contextPath() = '" + ctx.contextPath() + "'");
+        };
 
-        Spark.staticFiles.location("web");
-        setupHttpRouting();
+        app = Javalin.create()
+
+        // TODO? Spark.staticFiles.location("web");
+
+        // TODO Spark.webSocket("/ws", webSocketServer);
+
+        // TODO
+//        Spark.notFound((req, res) -> {
+//            String errMsg = String.format("[%s] %s not found", req.requestMethod(), req.pathInfo());
+//            return errorHandler(new Exception(errMsg), req, res);
+//        });
+
+                .exception(Exception.class, this::errorHandler)
+
+                .before(testHandler)
+
+                .before((ctx) -> System.out.println("Executing route: " + ctx.method().name() + " " + ctx.contextPath()))
+
+//                .delete("/db", clearApplicationHandler::handleRequest)
+//                .post("/user", registerHandler::handleRequest)
+//                .post("/session", loginHandler::handleRequest)
+//                .delete("/session", logoutHandler::handleRequest)
+//                .get("/game", listGamesHandler::handleRequest)
+//                .post("/game", createGameHandler::handleRequest)
+//                .put("/game", joinGameHandler::handleRequest)
+
+                .after((ctx) -> System.out.println("Finished executing route: " + ctx.contextPath()))
+
+                .start(desiredPort);
+
+        addShutdownHook();
 
         try {
             setupDataAccess();
         } catch (DataAccessException e) {
             System.out.println("Failed to setup data access!");
-            Spark.stop();
+            stop();
             System.exit(1);
         }
 
-        Spark.awaitInitialization();
-        int port = Spark.port();
+        // TODO?  Spark.awaitInitialization();
+        int port = app.port();
         System.out.println("Listening on port " + port);
         return port;
-    }
-
-    private void setupHttpRouting() {
-        exposeWebSocketServer();
-        createRoutes();
-        addShutdownHook();
     }
 
     private void setupDataAccess() throws DataAccessException {
@@ -88,17 +116,6 @@ public class Server {
         gameDAO.initialize();
     }
 
-    private void exposeWebSocketServer() {
-        Spark.webSocket("/ws", webSocketServer);
-    }
-
-    private void createRoutes() {
-        createErrorRoutes();
-        createBeforeRoutes();
-        createServiceRoutes();
-        createAfterRoutes();
-    }
-
     /**
      * Create a shutdown hook to ensure the server has a chance to complete any routes in progress and release resources
      * before shutting down.
@@ -106,45 +123,16 @@ public class Server {
     private void addShutdownHook() {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Stopping the server...");
-            Spark.stop();
+            stop();
             System.out.println("Server stopped");
         }));
     }
 
-    private void createErrorRoutes() {
-        // See web-api/example-code/.../Custom...Server2
-        Spark.notFound((req, res) -> {
-            String errMsg = String.format("[%s] %s not found", req.requestMethod(), req.pathInfo());
-            return errorHandler(new Exception(errMsg), req, res);
-        });
-        Spark.exception(Exception.class, this::errorHandler);
-    }
-
-    private void createBeforeRoutes() {
-        Spark.before((req, res) -> System.out.println("Executing route: " + req.requestMethod() + " " + req.pathInfo()));
-        // Filters take an optional pattern to restrict the routes to which they are applied:
-        // before("/protected/*", (req, res) -> {…});
-    }
-
-    private void createServiceRoutes() {
-        Spark.delete("/db", clearApplicationHandler::handleRequest);
-        Spark.post("/user", registerHandler::handleRequest);
-        Spark.post("/session", loginHandler::handleRequest);
-        Spark.delete("/session", logoutHandler::handleRequest);
-        Spark.get("/game", listGamesHandler::handleRequest);
-        Spark.post("/game", createGameHandler::handleRequest);
-        Spark.put("/game", joinGameHandler::handleRequest);
-    }
-
-    private void createAfterRoutes() {
-        Spark.after((req, res) -> System.out.println("Finished executing route: " + req.pathInfo()));
-    }
-
-    private Object errorHandler(Exception e, Request req, Response res) {
+    private void errorHandler(Exception e, Context ctx) {
         String msg = String.format("[ERROR] Unknown server error: %s", e.getMessage());
         System.out.println(msg);
         MessageResponse response = new MessageResponse(msg);
-        return HttpHandler.parseToBody(res, response, 500);
+        HttpHandler.parseToBody(ctx, response, 500);
     }
 
 }
