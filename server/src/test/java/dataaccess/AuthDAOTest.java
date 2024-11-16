6e29f2dd7767971fcbe595f15b8906dbfe8bcedd
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2024-11-16 16:07:40 -0700

    code quality: update constant names

--- server/src/test/java/dataaccess/AuthDAOTest.java
+++ server/src/test/java/dataaccess/AuthDAOTest.java
@@ -17,8 +17,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AuthDAOTest {
     private static final boolean IS_SQL_DAO = true;
-    private static final User validUser = new User("validUser", "password", "email");
-    private static final AuthToken validToken = new AuthToken("validTokenString", validUser.username());
+    private static final User VALID_USER = new User("validUser", "password", "email");
+    private static final AuthToken VALID_TOKEN = new AuthToken("validTokenString", VALID_USER.username());
     private static UserDAO userDAO;
     private static AuthDAO authDAO;
 
@@ -26,7 +26,7 @@ class AuthDAOTest {
     static void init() throws DataAccessException {
         userDAO = new MemoryUserDAO();
         userDAO.clearUsers();
-        userDAO.insertNewUser(validUser);
+        userDAO.insertNewUser(VALID_USER);
     }
 
     @AfterAll
@@ -43,33 +43,33 @@ class AuthDAOTest {
 
     @Test
     void addNewAuthTokenMakesTokenValid() throws DataAccessException {
-        authDAO.addAuthToken(validToken);
-        assertTrue(authDAO.isValidAuthToken(validToken.authToken()));
+        authDAO.addAuthToken(VALID_TOKEN);
+        assertTrue(authDAO.isValidAuthToken(VALID_TOKEN.authToken()));
     }
 
     @Test
     void addExistingAuthTokenThrows() throws DataAccessException {
-        authDAO.addAuthToken(validToken);
-        assertThrows(ValueAlreadyTakenException.class, () -> authDAO.addAuthToken(validToken));
+        authDAO.addAuthToken(VALID_TOKEN);
+        assertThrows(ValueAlreadyTakenException.class, () -> authDAO.addAuthToken(VALID_TOKEN));
     }
 
     @Test
     void addAuthTokenWithIncorrectUsernameThrows() {
-        AuthToken invalidToken = new AuthToken(validToken.authToken(), "invalidUsername");
+        AuthToken invalidToken = new AuthToken(VALID_TOKEN.authToken(), "invalidUsername");
         assertThrows(UnauthorizedAccessException.class, () -> authDAO.addAuthToken(invalidToken));
     }
 
     @Test
     void nonexistentTokenIsInvalid() throws DataAccessException {
-        AuthToken invalidToken = new AuthToken("invalidTokenString", validUser.username());
+        AuthToken invalidToken = new AuthToken("invalidTokenString", VALID_USER.username());
         assertFalse(authDAO.isValidAuthToken(invalidToken.authToken()));
     }
 
     @Test
     void removeAuthTokenMakesTokenInvalid() throws DataAccessException {
-        authDAO.addAuthToken(validToken);
-        authDAO.removeAuthToken(validToken.authToken());
-        assertFalse(authDAO.isValidAuthToken(validToken.authToken()));
+        authDAO.addAuthToken(VALID_TOKEN);
+        authDAO.removeAuthToken(VALID_TOKEN.authToken());
+        assertFalse(authDAO.isValidAuthToken(VALID_TOKEN.authToken()));
     }
 
     @Test
@@ -79,12 +79,12 @@ class AuthDAOTest {
 
     @Test
     void clearAuthTokensMakesTokensInvalid() throws DataAccessException {
-        authDAO.addAuthToken(validToken);
-        AuthToken validToken2 = new AuthToken("tokenString2", validUser.username());
+        authDAO.addAuthToken(VALID_TOKEN);
+        AuthToken validToken2 = new AuthToken("tokenString2", VALID_USER.username());
         authDAO.addAuthToken(validToken2);
 
         authDAO.clearAuthTokens();
-        assertFalse(authDAO.isValidAuthToken(validToken.authToken()));
+        assertFalse(authDAO.isValidAuthToken(VALID_TOKEN.authToken()));
         assertFalse(authDAO.isValidAuthToken(validToken2.authToken()));
     }
 
@@ -95,8 +95,8 @@ class AuthDAOTest {
 
     @Test
     void getUsernameReturnsCorrectUsername() throws DataAccessException {
-        authDAO.addAuthToken(validToken);
-        assertEquals(validUser.username(), authDAO.getUsername(validToken.authToken()));
+        authDAO.addAuthToken(VALID_TOKEN);
+        assertEquals(VALID_USER.username(), authDAO.getUsername(VALID_TOKEN.authToken()));
     }
 
     @Test
